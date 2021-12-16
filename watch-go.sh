17a232fbc4e39c00e90bf35e66a6b722634a1a41
yang <oseau@me.com>
2021-12-16 18:02:46 +0800

    update

--- watch-go.sh
+++ watch-go.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fswatch -e ".*" -i ".*/[^.]*\\.go$" -0 . | xargs -0 -n 1 -I {} sh -c "clear && make start && go run {} && make end"
+fswatch --exclude ".*" -i ".*/[^.]*\\.go$" -0 . | xargs -0 -n 1 -I {} sh -c "clear && make start && go run {} && make end"
